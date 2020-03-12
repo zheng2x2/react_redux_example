49e644d9c4e636ddbe94e869bc3f145a240df9f6
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-12 19:04:27 +0900

    Setup React web application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Root</h1>
+      <AddNumberRoot/>
+      <DisplayNumberRoot/>
+    </div>
+  );
+}
+
+export default App;
