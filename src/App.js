64315697d460cba54a8ff79011197acae94da131
zheng2x2 <kelly.xx.zheng@gmail.com>
2020-03-13 16:22:28 +0900

    Add Add and Display num Module for React

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import AddNumberRoot from "./components/AddNumberRoot";
 import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
-class App extends React.Component() {
+class App extends React.Component {
+  state = {
+    number : 0
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Root</h1>
-        <AddNumberRoot/>
-        <DisplayNumberRoot/>
+        <AddNumberRoot onClick={function(size){
+            this.setState({number:this.state.number + size})
+        }.bind(this)}/>
+        <DisplayNumberRoot number={this.state.number}/>
       </div>
     );
   }
